2183bf0c13c50bd0052cbe75a95fd78a2879e47c
KenzGooch <mackenziegooch@gmail.com>
2017-03-13 17:13:23 -0700

    round buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('#buttonStart').click( function () {
   $('#startScreen').fadeOut(1050);
 });
+$('.popup-buttons a').click( function(){
+    $('.hide').fadeOut(500);
+    $( $(this).attr('href') ).fadeIn(750);
+});
+$('.btn-close').click(
+  function(){
+    $(this).closest('.content-box').fadeOut(500);
+  }
+);
